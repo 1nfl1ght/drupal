6a764ff804bffe76a90511d76c912efb6c624100
1nfl1ght <pavelvinogradov2000@mail.ru>
2021-11-30 19:31:56 +0300

    popup done

--- #src/js/component.js
+++ #src/js/component.js
@@ -13,8 +13,31 @@ const Modal = ({
 };
 
 const App = () => {
-  const [modalActive, setModalActive] = React.useState(true);
-  return React.createElement(Modal, {
+  const [modalActive, setModalActive] = React.useState(false);
+  return React.createElement("div", null, React.createElement("div", {
+    className: "plans__items"
+  }, React.createElement(PlansItem, {
+    type: "item__start"
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => setModalActive(true)
+  })), React.createElement(PlansItem, {
+    type: "item__business"
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => setModalActive(true)
+  })), React.createElement(PlansItem, {
+    type: "item__vip"
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => setModalActive(true)
+  }))), React.createElement(Modal, {
     active: modalActive,
     setActive: setModalActive
   }, React.createElement("form", {
@@ -58,16 +81,46 @@ const App = () => {
     className: "form__button",
     type: "button",
     value: "ОСТАВИТЬ ЗАЯВКУ!"
-  }))));
+  })))));
 };
 
-const PlansBtn = () => {
-  return React.createElement("input", {
-    className: "plans__btn",
-    type: "button",
-    value: "ОСТАВИТЬ ЗАЯВКУ!"
-  });
+const PlansItem = ({
+  type,
+  children
+}) => {
+  return React.createElement("div", {
+    className: `${type} plans-item`
+  }, React.createElement("div", {
+    className: "plan-wrapper"
+  }, React.createElement("h3", null, "\u0411\u0438\u0437\u043D\u0435\u0441"), React.createElement("div", {
+    className: "line"
+  }), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u0420\u0430\u0437\u0440\u0430\u0431\u043E\u0442\u043A\u0430 \u0438 \u043F\u043E\u0434\u0434\u0435\u0440\u0436\u043A\u0430 Drupal")), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u041A\u043E\u043D\u0441\u0443\u043B\u044C\u0442\u0430\u0446\u0438\u0438 \u0438 \u0440\u0430\u0431\u043E\u0442\u044B \u043F\u043E SEO")), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u0423\u0441\u043B\u0443\u0433\u0438 \u0434\u0438\u0437\u0430\u0439\u043D\u0435\u0440\u0430")), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u0412\u044B\u0441\u043E\u043A\u043E\u0435 \u0432\u0440\u0435\u043C\u044F \u0440\u0435\u0430\u043A\u0446\u0438\u0438 \u2013 \u0434\u043E 2 \u0440\u0430\u0431\u043E\u0447\u0438\u0445 \u0434\u043D\u0435\u0439")), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u041D\u0435\u0438\u0441\u043F\u043E\u043B\u044C\u0437\u043E\u0432\u0430\u043D\u043D\u044B\u0435 \u0447\u0430\u0441\u044B \u043D\u0435 \u043F\u0435\u0440\u0435\u043D\u043E\u0441\u044F\u0442\u0441\u044F")), React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("div", {
+    className: "checkmark"
+  }), React.createElement("p", null, "\u041F\u0440\u0435\u0434\u043E\u043F\u043B\u0430\u0442\u0430 \u043E\u0442 30 000 \u0440\u0443\u0431\u043B\u0435\u0439 \u0432 \u043C\u0435\u0441\u044F\u0446")), React.createElement("div", {
+    id: "plans__button"
+  }, children)));
 };
 
-ReactDOM.render(React.createElement(PlansBtn, null), document.getElementById("plans__button"));
-ReactDOM.render(React.createElement(App, null), document.getElementById("root"));
+ReactDOM.render(React.createElement(App, null), document.querySelector(".plans__place"));
--- #src/jsx/component.jsx
+++ #src/jsx/component.jsx
@@ -9,40 +9,93 @@ const Modal = ({active, setActive, children}) => {
 };
 
 const App = () => {
-    const [modalActive, setModalActive] = React.useState(true);
+    const [modalActive, setModalActive] = React.useState(false);
     return (
-        <Modal active={modalActive} setActive={setModalActive}>
-            <form action="">
-                <div>
-                    <input className="form__elem" id="name" type="text" name="name" required placeholder="Ваше имя"/>
-                </div>
-                <div>
-                    <input className="form__elem" id="phone" type="tel" name="phone" required placeholder="Телефон"/>
-                </div> 
-                <div> 
-                    <input className="form__elem" id="email" type="email" name="email" required placeholder="E-mail"/>
-                </div>
-                <div>
-                    <input className="comment form__elem" type="text" placeholder="Ваш комментарий"/>
-                </div>
-                <div id="modal__form__checkbox">
-                    <input className="checkbox__input" type="checkbox" id="modal__userAgreement"/>
-                    <label className="checkbox__label" htmlFor="userAgreement" htmlFor="modal__userAgreement">Отправляя заявку я даю согласие на <a>обработку своих персональных данных</a>.<span>*</span></label>
-                </div>
-
-                <div>
-                    <input className="form__button" type="button" value="ОСТАВИТЬ ЗАЯВКУ!"/>
-                </div>
-            </form>
-        </Modal>
+        <div>
+            <div className="plans__items">
+                <PlansItem type="item__start" ><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
+                <PlansItem type="item__business" ><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
+                <PlansItem type="item__vip" ><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
+            </div>
+            <Modal active={modalActive} setActive={setModalActive}>
+                <form action="">
+                    <div>
+                        <input className="form__elem" id="name" type="text" name="name" required placeholder="Ваше имя"/>
+                    </div>
+                    <div>
+                        <input className="form__elem" id="phone" type="tel" name="phone" required placeholder="Телефон"/>
+                    </div> 
+                    <div> 
+                        <input className="form__elem" id="email" type="email" name="email" required placeholder="E-mail"/>
+                    </div>
+                    <div>
+                        <input className="comment form__elem" type="text" placeholder="Ваш комментарий"/>
+                    </div>
+                    <div id="modal__form__checkbox">
+                        <input className="checkbox__input" type="checkbox" id="modal__userAgreement"/>
+                        <label className="checkbox__label" htmlFor="userAgreement" htmlFor="modal__userAgreement">Отправляя заявку я даю согласие на <a>обработку своих персональных данных</a>.<span>*</span></label>
+                    </div>
+                    <div>
+                        <input className="form__button" type="button" value="ОСТАВИТЬ ЗАЯВКУ!"/>
+                    </div>
+                </form>
+            </Modal>
+        </div>
+        
     );
 };
 
-const PlansBtn = () => {
+const PlansItem = ({type, children}) => {
     return (
-        <input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!"/>
+        <div className={`${type} plans-item`}>
+            <div className="plan-wrapper">
+                <h3>
+                    Бизнес
+                </h3>
+                <div className="line"></div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Разработка и поддержка Drupal</p>
+                </div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Консультации и работы по SEO</p>                    
+                </div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Услуги дизайнера</p>
+                </div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Высокое время реакции –
+                        до 2 рабочих дней</p>
+                </div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Неиспользованные часы не переносятся</p>
+                </div>
+                <div className="item__option">
+                    <div className="checkmark">
+                        {/* @@include('../img/svg/Vector.svg') */}
+                    </div>
+                    <p>Предоплата от 30 000 рублей в месяц</p>
+                </div>
+                <div id="plans__button">
+                {children}
+                </div>
+            </div>
+        </div>
     );
 }
 
-ReactDOM.render(<PlansBtn />, document.getElementById("plans__button"));
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.querySelector(".plans__place"));
