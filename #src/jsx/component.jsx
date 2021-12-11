32ca87ebcb44ee2a13e1134885301e54001a502e
1nfl1ght <pavelvinogradov2000@mail.ru>
2021-12-11 17:05:58 +0300

    rebuild form as react component; fix page scale; requestAnimationFrame popup

--- #src/jsx/component.jsx
+++ #src/jsx/component.jsx
@@ -1,47 +1,15 @@
-const Modal = ({active, setActive, children}) => {
+const Modal = ({children}) => {
+    const hideModal = () => {
+        let modal = document.querySelector(".modal");
+        modal.style.transform = "scale(0)";
+    }
+
     return (
-        <div className={active ? "modal active" : "modal"} onClick={() => setActive(false)}>
+        <div className="modal" onClick={() => hideModal()}>
             <div className="body__form" onClick={e => e.stopPropagation()}>
                 {children}
             </div>
         </div>
-    );
-};
-
-const App = () => {
-    const [modalActive, setModalActive] = React.useState(false);
-    return (
-        <div>
-            <div className="plans__items">
-                <PlansItem type="item__start" textFrom={item__start} n={4}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
-                <PlansItem type="item__business" textFrom={item__business} n={5}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
-                <PlansItem type="item__vip" textFrom={item__vip} n={5}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => setModalActive(true)}/></PlansItem>
-            </div>
-            <Modal active={modalActive} setActive={setModalActive}>
-                <form action="">
-                    <div>
-                        <input className="form__elem" id="name" type="text" name="name" required placeholder="Ваше имя"/>
-                    </div>
-                    <div>
-                        <input className="form__elem" id="phone" type="tel" name="phone" required placeholder="Телефон"/>
-                    </div> 
-                    <div> 
-                        <input className="form__elem" id="email" type="email" name="email" required placeholder="E-mail"/>
-                    </div>
-                    <div>
-                        <input className="comment form__elem" type="text" placeholder="Ваш комментарий"/>
-                    </div>
-                    <div id="modal__form__checkbox">
-                        <input className="checkbox__input" type="checkbox" id="modal__userAgreement"/>
-                        <label className="checkbox__label" htmlFor="userAgreement" htmlFor="modal__userAgreement">Отправляя заявку я даю согласие на <a>обработку своих персональных данных</a>.<span>*</span></label>
-                    </div>
-                    <div>
-                        <input className="form__button" type="button" value="ОСТАВИТЬ ЗАЯВКУ!"/>
-                    </div>
-                </form>
-            </Modal>
-        </div>
-        
     );
 };
 
@@ -53,48 +21,86 @@ const Option = ({text}) => {
     );
 };
 
-const item__start = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Неиспользованные оплаченные часы переносятся на следующий месяц', 'Предоплата от 6 000 реблей в месяц'];
-const item__business = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - до 2 рабочих дней', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 30 000 реблей в месяц'];
-const item__vip = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - в день обращения', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 270 000 реблей в месяц'];
+const itemStart = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Неиспользованные оплаченные часы переносятся на следующий месяц', 'Предоплата от 6 000 реблей в месяц'];
+const itemBusiness = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - до 2 рабочих дней', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 30 000 реблей в месяц'];
+const itemVip = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - в день обращения', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 270 000 реблей в месяц'];
+const startTitle = ['Стартовый'];
+const businessTitle = ['Бизнес'];
+const vipTitle = ['VIP'];
 
-const PlansItem = ({type, textFrom, n, children}) => {
+const PlansItem = ({type, textFrom, titleFrom, n, children}) => {
     return (
         <div className={`${type} plans-item`}>
             <div className="plan-wrapper">
                 <h3>
-                    Бизнес
+                {titleFrom}
                 </h3>
                 <div className="line"></div>
                 <div>
                 { [...Array(n)].map((item, index) => <Option text={textFrom[index]} key={index} /> ) }
                 </div>
-                <div id="plans__button">
                 {children}
-                </div>
             </div>
         </div>
     );
 };
 
-{/* <div className="item__option">
-                    <p>Разработка и поддержка Drupal</p>
-                </div>
-                <div className="item__option">
-                    <p>Консультации и работы по SEO</p>
-                </div>
-                <div className="item__option">
-                    <p>Услуги дизайнера</p>
-                </div>
-                <div className="item__option">
-                    <p>Высокое время реакции –
-                        до 2 рабочих дней</p>
-                </div>
-                <div className="item__option">
-                    <p>Неиспользованные часы не переносятся</p>
-                </div>
-                <div className="item__option">
-                    <p>Предоплата от 30 000 рублей в месяц</p>
-</div> */}
+const Form = ({ checkBlockId,  checkBoxId }) => {
+    return (
+        <form action="">
+            <div>
+                <input className="form__elem" id="name" type="text" name="name" required placeholder="Ваше имя"/>
+            </div>
+            <div>
+                <input className="form__elem" id="phone" type="tel" name="phone" required placeholder="Телефон"/>
+            </div> 
+            <div> 
+                <input className="form__elem" id="email" type="email" name="email" required placeholder="E-mail"/>
+            </div>
+            <div>
+                <input className="comment form__elem" type="text" placeholder="Ваш комментарий"/>
+            </div>
+            <div id={checkBlockId}>
+                <input className="checkbox__input" type="checkbox" id={checkBoxId}/>
+                <label className="checkbox__label" htmlFor={checkBoxId}>Отправляя заявку я даю согласие на <a>обработку своих персональных данных</a>.<span>*</span></label>
+            </div>
+            <div className="g-recaptcha" data-sitekey="6LfvTFQdAAAAAMi9xDDFtM63vjBlBemc9O00S52m"></div>
+            <div>
+                <input className="form__button" type="button" value="ОСТАВИТЬ ЗАЯВКУ!"/>
+            </div>
+        </form>
+    );
+};
 
+const App = () => {
+    const showModal = () => {
+        let start = Date.now();
+        let modal = document.querySelector(".modal");
+        let timer = requestAnimationFrame(function animateBall(timestamp) {
+            let interval = Date.now() - start;
+    
+            modal.style.transform = "scale(" + (interval/200) + ")";
+    
+            if (interval < 200) requestAnimationFrame(animateBall);
+    
+        });
+    }
+
+    return (
+        <div>
+            <div className="plans__items">
+                <PlansItem type="item__start" textFrom={itemStart} titleFrom={startTitle} n={4}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => showModal()}/></PlansItem>
+                <PlansItem type="item__business" textFrom={itemBusiness} titleFrom={businessTitle} n={5}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => showModal()}/></PlansItem>
+                <PlansItem type="item__vip" textFrom={itemVip} titleFrom={vipTitle} n={5}><input className="plans__btn" type="button" value="ОСТАВИТЬ ЗАЯВКУ!" onClick={() => showModal()}/></PlansItem>
+            </div>
+            <Modal>
+                <Form checkBlockId={"modal__form__checkbox"} checkBoxId={"modal__userAgreement"}/>
+            </Modal>
+        </div>
+        
+    );
+};
+
+ReactDOM.render(<Form checkBlockId={"form__checkbox"} checkBoxId={"userAgreement"}/>, document.querySelector("#main-form"));
 
 ReactDOM.render(<App />, document.querySelector(".plans__place"));
--- dist/js/script.js
+++ dist/js/script.js
@@ -110,54 +110,61 @@ $(document).ready(function () {
     });
 })
 const Modal = ({
-  active,
-  setActive,
   children
 }) => {
+  const hideModal = () => {
+    let modal = document.querySelector(".modal");
+    modal.style.transform = "scale(0)";
+  };
+
   return React.createElement("div", {
-    className: active ? "modal active" : "modal",
-    onClick: () => setActive(false)
+    className: "modal",
+    onClick: () => hideModal()
   }, React.createElement("div", {
     className: "body__form",
     onClick: e => e.stopPropagation()
   }, children));
 };
 
-const App = () => {
-  const [modalActive, setModalActive] = React.useState(false);
-  return React.createElement("div", null, React.createElement("div", {
-    className: "plans__items"
-  }, React.createElement(PlansItem, {
-    type: "item__start",
-    textFrom: item__start,
-    n: 4
-  }, React.createElement("input", {
-    className: "plans__btn",
-    type: "button",
-    value: "ОСТАВИТЬ ЗАЯВКУ!",
-    onClick: () => setModalActive(true)
-  })), React.createElement(PlansItem, {
-    type: "item__business",
-    textFrom: item__business,
-    n: 5
-  }, React.createElement("input", {
-    className: "plans__btn",
-    type: "button",
-    value: "ОСТАВИТЬ ЗАЯВКУ!",
-    onClick: () => setModalActive(true)
-  })), React.createElement(PlansItem, {
-    type: "item__vip",
-    textFrom: item__vip,
-    n: 5
-  }, React.createElement("input", {
-    className: "plans__btn",
-    type: "button",
-    value: "ОСТАВИТЬ ЗАЯВКУ!",
-    onClick: () => setModalActive(true)
-  }))), React.createElement(Modal, {
-    active: modalActive,
-    setActive: setModalActive
-  }, React.createElement("form", {
+const Option = ({
+  text
+}) => {
+  return React.createElement("div", {
+    className: "item__option"
+  }, React.createElement("p", null, text));
+};
+
+const itemStart = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Неиспользованные оплаченные часы переносятся на следующий месяц', 'Предоплата от 6 000 реблей в месяц'];
+const itemBusiness = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - до 2 рабочих дней', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 30 000 реблей в месяц'];
+const itemVip = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - в день обращения', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 270 000 реблей в месяц'];
+const startTitle = ['Стартовый'];
+const businessTitle = ['Бизнес'];
+const vipTitle = ['VIP'];
+
+const PlansItem = ({
+  type,
+  textFrom,
+  titleFrom,
+  n,
+  children
+}) => {
+  return React.createElement("div", {
+    className: `${type} plans-item`
+  }, React.createElement("div", {
+    className: "plan-wrapper"
+  }, React.createElement("h3", null, titleFrom), React.createElement("div", {
+    className: "line"
+  }), React.createElement("div", null, [...Array(n)].map((item, index) => React.createElement(Option, {
+    text: textFrom[index],
+    key: index
+  }))), children));
+};
+
+const Form = ({
+  checkBlockId,
+  checkBoxId
+}) => {
+  return React.createElement("form", {
     action: ""
   }, React.createElement("div", null, React.createElement("input", {
     className: "form__elem",
@@ -185,73 +192,75 @@ const App = () => {
     type: "text",
     placeholder: "Ваш комментарий"
   })), React.createElement("div", {
-    id: "modal__form__checkbox"
+    id: checkBlockId
   }, React.createElement("input", {
     className: "checkbox__input",
     type: "checkbox",
-    id: "modal__userAgreement"
+    id: checkBoxId
   }), React.createElement("label", {
     className: "checkbox__label",
-    htmlFor: "userAgreement",
-    htmlFor: "modal__userAgreement"
-  }, "\u041E\u0442\u043F\u0440\u0430\u0432\u043B\u044F\u044F \u0437\u0430\u044F\u0432\u043A\u0443 \u044F \u0434\u0430\u044E \u0441\u043E\u0433\u043B\u0430\u0441\u0438\u0435 \u043D\u0430 ", React.createElement("a", null, "\u043E\u0431\u0440\u0430\u0431\u043E\u0442\u043A\u0443 \u0441\u0432\u043E\u0438\u0445 \u043F\u0435\u0440\u0441\u043E\u043D\u0430\u043B\u044C\u043D\u044B\u0445 \u0434\u0430\u043D\u043D\u044B\u0445"), ".", React.createElement("span", null, "*"))), React.createElement("div", null, React.createElement("input", {
+    htmlFor: checkBoxId
+  }, "\u041E\u0442\u043F\u0440\u0430\u0432\u043B\u044F\u044F \u0437\u0430\u044F\u0432\u043A\u0443 \u044F \u0434\u0430\u044E \u0441\u043E\u0433\u043B\u0430\u0441\u0438\u0435 \u043D\u0430 ", React.createElement("a", null, "\u043E\u0431\u0440\u0430\u0431\u043E\u0442\u043A\u0443 \u0441\u0432\u043E\u0438\u0445 \u043F\u0435\u0440\u0441\u043E\u043D\u0430\u043B\u044C\u043D\u044B\u0445 \u0434\u0430\u043D\u043D\u044B\u0445"), ".", React.createElement("span", null, "*"))), React.createElement("div", {
+    className: "g-recaptcha",
+    "data-sitekey": "6LfvTFQdAAAAAMi9xDDFtM63vjBlBemc9O00S52m"
+  }), React.createElement("div", null, React.createElement("input", {
     className: "form__button",
     type: "button",
     value: "ОСТАВИТЬ ЗАЯВКУ!"
-  })))));
+  })));
 };
 
-const Option = ({
-  text
-}) => {
-  return React.createElement("div", {
-    className: "item__option"
-  }, React.createElement("p", null, text));
+const App = () => {
+  const showModal = () => {
+    let start = Date.now();
+    let modal = document.querySelector(".modal");
+    let timer = requestAnimationFrame(function animateBall(timestamp) {
+      let interval = Date.now() - start;
+      modal.style.transform = "scale(" + interval / 200 + ")";
+      if (interval < 200) requestAnimationFrame(animateBall);
+    });
+  };
+
+  return React.createElement("div", null, React.createElement("div", {
+    className: "plans__items"
+  }, React.createElement(PlansItem, {
+    type: "item__start",
+    textFrom: itemStart,
+    titleFrom: startTitle,
+    n: 4
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => showModal()
+  })), React.createElement(PlansItem, {
+    type: "item__business",
+    textFrom: itemBusiness,
+    titleFrom: businessTitle,
+    n: 5
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => showModal()
+  })), React.createElement(PlansItem, {
+    type: "item__vip",
+    textFrom: itemVip,
+    titleFrom: vipTitle,
+    n: 5
+  }, React.createElement("input", {
+    className: "plans__btn",
+    type: "button",
+    value: "ОСТАВИТЬ ЗАЯВКУ!",
+    onClick: () => showModal()
+  }))), React.createElement(Modal, null, React.createElement(Form, {
+    checkBlockId: "modal__form__checkbox",
+    checkBoxId: "modal__userAgreement"
+  })));
 };
 
-const item__start = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Неиспользованные оплаченные часы переносятся на следующий месяц', 'Предоплата от 6 000 реблей в месяц'];
-const item__business = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - до 2 рабочих дней', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 30 000 реблей в месяц'];
-const item__vip = ['Консультации и работы по СЕО', 'Услуги дизайнера', 'Высокое время реакции - в день обращения', 'Неиспользованные оплаченные часы не переносятся', 'Предоплата от 270 000 реблей в месяц'];
-
-const PlansItem = ({
-  type,
-  textFrom,
-  n,
-  children
-}) => {
-  return React.createElement("div", {
-    className: `${type} plans-item`
-  }, React.createElement("div", {
-    className: "plan-wrapper"
-  }, React.createElement("h3", null, "\u0411\u0438\u0437\u043D\u0435\u0441"), React.createElement("div", {
-    className: "line"
-  }), React.createElement("div", null, [...Array(n)].map((item, index) => React.createElement(Option, {
-    text: textFrom[index],
-    key: index
-  }))), React.createElement("div", {
-    id: "plans__button"
-  }, children)));
-};
-
-{
-  /* <div className="item__option">
-                     <p>Разработка и поддержка Drupal</p>
-                 </div>
-                 <div className="item__option">
-                     <p>Консультации и работы по SEO</p>
-                 </div>
-                 <div className="item__option">
-                     <p>Услуги дизайнера</p>
-                 </div>
-                 <div className="item__option">
-                     <p>Высокое время реакции –
-                         до 2 рабочих дней</p>
-                 </div>
-                 <div className="item__option">
-                     <p>Неиспользованные часы не переносятся</p>
-                 </div>
-                 <div className="item__option">
-                     <p>Предоплата от 30 000 рублей в месяц</p>
-  </div> */
-}
+ReactDOM.render(React.createElement(Form, {
+  checkBlockId: "form__checkbox",
+  checkBoxId: "userAgreement"
+}), document.querySelector("#main-form"));
 ReactDOM.render(React.createElement(App, null), document.querySelector(".plans__place"));
